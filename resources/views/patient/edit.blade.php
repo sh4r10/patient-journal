a058b4e041cf7df7141e3482dd7cd8c7ae6f71ef
Shariq Shahbaz <iamsh4r10@gmail.com>
2024-04-20 01:14:03 +0200

    fix name truncation in patient edit view, closes #4

--- resources/views/patient/edit.blade.php
+++ resources/views/patient/edit.blade.php
@@ -13,13 +13,13 @@
                         <div class="label">
                             <span class="label-text">Name</span>
                         </div>
-                        <input type="text" name="name" value={{$patient->name}} class="input input-bordered w-full" />
+                        <input type="text" name="name" value="{{$patient->name}}" class="input input-bordered w-full" />
                     </label>
                     <label class="form-control w-full">
                         <div class="label">
                             <span class="label-text">Personnummer</span>
                         </div>
-                        <input type="text" name="personnummer" value={{$patient->personnummer}} class="input input-bordered w-full" />
+                        <input type="text" name="personnummer" value="{{$patient->personnummer}}" class="input input-bordered w-full" />
                     </label>
                 </div>
                 <div class="flex gap-6">
@@ -27,13 +27,13 @@
                         <div class="label">
                             <span class="label-text">Email</span>
                         </div>
-                        <input type="text" name="email" value={{$patient->email}} class="input input-bordered w-full" />
+                        <input type="text" name="email" value="{{$patient->email}}" class="input input-bordered w-full" />
                     </label>
                     <label class="form-control w-full">
                         <div class="label">
                             <span class="label-text">Phone</span>
                         </div>
-                        <input type="text" name="phone" value={{$patient->phone}} class="input input-bordered w-full" />
+                        <input type="text" name="phone" value="{{$patient->phone}}" class="input input-bordered w-full" />
                     </label>
                 </div>
                 <div class="w-full flex flex-row-reverse justify-start items-center gap-4 mt-4">
